3e762f9b4bb1a6ff91d1b5c60be63b3f2bf3586b
Cuerci <lucas.cuerci25@gmail.com>
2022-05-27 12:02:54 -0300

    add controller model and view

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,30 @@
+class MessagesController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+        def set_message
+        end
+
+        def message_params
+        end
+end

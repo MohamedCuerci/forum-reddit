8cc6b5095eabe619c8fd2fb33d6bd07e7f267050
Cuerci <lucas.cuerci25@gmail.com>
2022-05-27 13:09:26 -0300

    criação das views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,30 +1,49 @@
 class MessagesController < ApplicationController
+    before_action :set_message, only: [:show, :edit, :update, :destroy ]
 
     def index
+        @messages = Message.all.order("created_at DESC")
     end
 
     def show
     end
 
     def new
+        @message = Message.new
     end
 
     def create
+        @message = Message.new(message_params)
+
+        if @message.save
+            redirect_to @message
+        else
+            redirect_to :new, status: :unprocessable_entity
+        end
     end
 
     def edit
     end
 
     def update
+        if @message.update(message_params)
+            redirect_to @message
+        else
+            render :edit, status: :unprocessable_entity
+        end
     end
 
     def destroy
+        @message.destroy
+        redirect_to root_path, status: :see_other
     end
 
     private
         def set_message
+            @message = Message.find(params[:id])
         end
 
         def message_params
+            params.require(:message).permit(:title, :description)
         end
 end
